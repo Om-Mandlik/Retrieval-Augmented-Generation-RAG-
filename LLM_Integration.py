e718fcdddeb21671c41b855b87149a0b255d30a2
Om Mandlik <ommandlik702@gmail.com>
2026-07-18 12:31:58 +0000

    Attached LLM to RAG

---
+++ LLM_Integration.py
@@ -0,0 +1,23 @@
+from Retrival_pipeline import rag_retriever
+
+from google import genai
+import os
+from dotenv import load_dotenv
+
+GEMINI_API_KEY="AIzaSyABH4V7qz4fGbQqM-TkurnDGL_0JsqbVRg"
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+
+def rag_simple(query,retriver,llm,top_k=3):
+    retrieved_docs = retriver.retrieve(query, top_K=top_k)
+    context = "\n\n".join([doc['content'] for doc in retrieved_docs]) if retriver else ""
+    if not context:
+        return "no relevent context is found"
+    prompt = f"Use the following context to answer the question concisely.Context:{context} Question {query}"
+
+    response = client.models.generate_content(model="gemini-3.5-flash",contents=prompt)
+    return response.text
+
+answer = rag_simple("What is attention mechanism ",rag_retriever,client)
+print(answer)
+
